b312e1c88ff1a09f259f50d2ee6a6ad8d42ac08b
Natasha Radika <81007696+tashagrc@users.noreply.github.com>
2025-01-17 09:51:55 +0700

    Initial Commit

---
+++ aura/auraApp.swift
@@ -0,0 +1,17 @@
+//
+//  auraApp.swift
+//  aura
+//
+//  Created by Natasha Radika on 17/01/25.
+//
+
+import SwiftUI
+
+@main
+struct auraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ auraTests/auraTests.swift
@@ -0,0 +1,17 @@
+//
+//  auraTests.swift
+//  auraTests
+//
+//  Created by Natasha Radika on 17/01/25.
+//
+
+import Testing
+@testable import aura
+
+struct auraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
